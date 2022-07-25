05f63627069489e640d3a6b1a08626a9a75f9f73
zhy <920462849@qq.com>
2022-07-25 15:15:18 +0800

    添加文件

---
+++ HelloGit/src/main/java/org/zhy/git/HelloGit.java
@@ -0,0 +1,13 @@
+package org.zhy.git;
+
+/**
+ * @author zhy
+ * @create 2022-07-25 15:05
+ */
+public class HelloGit {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Git!");
+    }
+}
